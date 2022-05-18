744be0258aa0defd02db940790e8eaee71317e96
Zake77 <izakesz@gmail.com>
2022-05-18 14:55:02 -0300

    atividades curso DH

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8f/e07d48f7c4d6001c174e905c0a3ce5f5
@@ -0,0 +1,16 @@
+package projetodh;
+
+public class projetoArrayUsual {
+
+	public static void main(String[] args) {
+		
+		String[] cores = new String[] {"preto", "branco", "azul", "amarelo"};
+		
+		for (String cor : cores) {
+			System.out.println(cor);
+			
+		}
+		
+	}
+
+}
---
+++ projetodh/src/projetodh/atividadeVetor.java
@@ -0,0 +1,19 @@
+package projetodh;
+
+public class atividadeVetor {
+
+	public static void main(String[] args) {
+		
+		String[] frutas = new String[3];
+		
+		frutas[0] = "Abacate";
+		frutas[1] = "Mamão";
+		frutas[2] = "Banana";
+		
+		for (String fruta : frutas) {
+			System.out.println(fruta);
+		}
+
+	}
+
+}
